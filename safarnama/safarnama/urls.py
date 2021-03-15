7bd5b57d67ce9262899f6a0a3133dd0ae4c7a5af
kirtiwardhan <rawal.1816@gmail.com>
2021-03-15 07:50:55 +0530

    Initial Commit

--- safarnama/safarnama/urls.py
+++ safarnama/safarnama/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('App01.urls')),
+    path('', include('TravelGO.urls')),
     path('admin/', admin.site.urls),
 ]
